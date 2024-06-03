96b2e6aeb507f43cbdbb6df9a6f47bdc61dd1a66
Jasse Merivirta <jayzstep@gmail.com>
2024-06-03 11:25:06 +0300

    added train or run args for usability

--- NumNet/numnet/nn.py
+++ NumNet/numnet/nn.py
@@ -1,22 +1,27 @@
+import pickle
 import random
+import argparse
+import gradio as gr
 import numpy as np
 import pandas as pd
-import gradio as gr
 from PIL import Image
 
 
 def cost_derivative(output_activations, y):
-    return (output_activations-y)
+    return output_activations - y
+
 
 def sigmoid(z):
-    return 1.0/(1.0+np.exp(-z))
+    return 1.0 / (1.0 + np.exp(-z))
+
 
 def sigmoid_prime(z):
-    return sigmoid(z)*(1-sigmoid(z))
+    return sigmoid(z) * (1 - sigmoid(z))
 
-class NN():
+
+class NN:
     def __init__(self, layers, activation, activation_derivative, cost_derivative):
-        self.b = [np.random.randn(y,1) for y in layers[1:]]
+        self.b = [np.random.randn(y, 1) for y in layers[1:]]
         self.w = [np.random.randn(y, x) for x, y in zip(layers[:-1], layers[1:])]
         self.activation = activation
         self.activation_derivative = activation_derivative
@@ -24,7 +29,7 @@ class NN():
 
     def predict(self, a):
         if len(a) != 784:
-            raise ValueError('Wrong input size.')
+            raise ValueError("Wrong input size.")
         for b, w in zip(self.b, self.w):
             a = self.activation(np.dot(w, a) + b)
         return a
@@ -49,19 +54,19 @@ class NN():
                 zs.append(z1)
                 a = self.activation(z1)
                 all_as.append(a)
-                z2 = np.dot(w2, a) +b2
+                z2 = np.dot(w2, a) + b2
                 zs.append(z2)
                 a = self.activation(z2)
                 all_as.append(a)
 
                 # backward
                 # output layer:
-                d2 = self.cost_derivative(all_as[-1], y) # 10x1
-                ad3 = self.activation_derivative(zs[-1]) # 10x1
-                d2 = d2 * ad3 # 10x1
-                dz2 = all_as[-2] # 30x1
-                nabla_w2 = np.dot(d2, dz2.T) # 30x10
-                nabla_b2 = d2 # 10x1
+                d2 = self.cost_derivative(all_as[-1], y)  # 10x1
+                ad3 = self.activation_derivative(zs[-1])  # 10x1
+                d2 = d2 * ad3  # 10x1
+                dz2 = all_as[-2]  # 30x1
+                nabla_w2 = np.dot(d2, dz2.T)  # 30x10
+                nabla_b2 = d2  # 10x1
 
                 # hidden layer:
                 d1 = np.dot(w2.T, d2) * self.activation_derivative(zs[-2])
@@ -75,47 +80,75 @@ class NN():
 
                 self.b[0] -= learning_rate * nabla_b1
                 self.b[1] -= learning_rate * nabla_b2
-                
+
             if test_data:
                 print(f"Epoch {i}: {self.evaluate(test_data)} / 10000")
 
-
     def evaluate(self, test_data):
         test_results = [(np.argmax(self.predict(x)), y) for (x, y) in test_data]
-        return sum(int(x==y) for x, y in test_results)
+        return sum(int(x == y) for x, y in test_results)
+
 
 def preprocess_image(image_dict):
-    image_data = image_dict['composite']
-    image = Image.fromarray(image_data).convert('L')
-    image = np.array(image).astype('float32') / 255
+    image_data = image_dict["composite"]
+    image = Image.fromarray(image_data).convert("L")
+    image = np.array(image).astype("float32") / 255
     image = image.reshape(784, 1)
     return image
+
 
 def predict_digit(image_dict):
     preprocessed_image = preprocess_image(image_dict)
     prediction = net.predict(preprocessed_image)
     return int(np.argmax(prediction))
 
+
+def save_weights_and_biases(model, filename):
+    with open(filename, "wb") as file:
+        pickle.dump((model.w, model.b), file)
+
+
+def load_weights_and_biases(filename):
+    with open(filename, "rb") as file:
+        w, b = pickle.load(file)
+    model = NN([784, 30, 10], sigmoid, sigmoid_prime, cost_derivative)
+    model.w = w
+    model.b = b
+    return model
+
+
 if __name__ == "__main__":
-    train_data = pd.read_csv('../data/mnist_train.csv')
-    test_data = pd.read_csv('../data/mnist_test.csv')
+    parser = argparse.ArgumentParser(description="Choose train or run")
+    parser.add_argument('mode', choices=['train', 'run'], help="'train' to train and save weights/biases to memory and start the app, 'run' to load weights and biases from memory and start the app.")
+    args = parser.parse_args()
 
-    x_train = train_data.iloc[:, 1:].values.astype('float32') / 255
-    y_train = train_data.iloc[:, 0].values
-    x_test = test_data.iloc[:, 1:].values.astype('float32') / 255
-    y_test = test_data.iloc[:, 0].values
+    if args.mode == 'train':
+        train_data = pd.read_csv("../data/mnist_train.csv")
+        test_data = pd.read_csv("../data/mnist_test.csv")
 
-    y_train_one_hot = np.eye(10)[y_train]
+        x_train = train_data.iloc[:, 1:].values.astype("float32") / 255
+        y_train = train_data.iloc[:, 0].values
+        x_test = test_data.iloc[:, 1:].values.astype("float32") / 255
+        y_test = test_data.iloc[:, 0].values
 
-    training_data = [(x.reshape(-1, 1), y.reshape(-1, 1)) for x, y in zip(x_train, y_train_one_hot)]
-    test_data = [(x.reshape(-1, 1), y) for x, y in zip(x_test, y_test)]
+        y_train_one_hot = np.eye(10)[y_train]
 
-    net = NN([784,30,10], sigmoid, sigmoid_prime, cost_derivative)
-    net.train(training_data, 30, 3, test_data)
+        training_data = [
+            (x.reshape(-1, 1), y.reshape(-1, 1)) for x, y in zip(x_train, y_train_one_hot)
+        ]
+        test_data = [(x.reshape(-1, 1), y) for x, y in zip(x_test, y_test)]
+
+        net = NN([784, 30, 10], sigmoid, sigmoid_prime, cost_derivative)
+        net.train(training_data, 30, 0.3, test_data)
+        save_weights_and_biases(net, 'weights_and_biases.pkl')
+
+    elif args.mode == 'run':
+        net = load_weights_and_biases('weights_and_biases.pkl')
+
     demo = gr.Interface(
         fn=predict_digit,
         # inputs= gr.ImageEditor(sources=(), image_mode='L', crop_size=(28,28)),
-        inputs= gr.ImageEditor(sources=(), crop_size=(28,28)),
-        outputs=gr.Label(num_top_classes=3)
+        inputs=gr.ImageEditor(sources=(), crop_size=(28, 28)),
+        outputs=gr.Label(num_top_classes=3),
     )
     demo.launch()
